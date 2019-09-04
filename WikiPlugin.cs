8faf03e27b2e6eeb7e95886f8aa5cf11fc07e98a
Katelyn Gigante <clockwork.singularity@gmail.com>
2019-09-05 00:26:05 +1000

    owo what's this?

---
+++ WikiPlugin.cs
@@ -0,0 +1,55 @@
+﻿using Dalamud.Game.Internal.Network;
+using Dalamud.Plugin;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace Silasary.WikiPlugin
+{
+    public class WikiPlugin : IDalamudPlugin
+    {
+        private DalamudPluginInterface dalamud;
+
+        public string Name => "Wiki Plugin";
+
+        public void Dispose()
+        {
+            dalamud.Framework.Network.OnZonePacket -= processPacket;
+        }
+
+        public void Initialize(DalamudPluginInterface pluginInterface)
+        {
+            this.dalamud = pluginInterface ?? throw new ArgumentNullException(nameof(pluginInterface));
+            pluginInterface.Framework.Network.OnZonePacket += processPacket;
+            dalamud.Framework.Gui.Chat.Print($"Registered WikiPlugin");
+        }
+
+        private void processPacket(IntPtr dataPtr)
+        {
+            var ptype = Marshal.ReadInt16(dataPtr, 2);
+            if (ptype == 120)
+            {
+                byte[] packet = new byte[64];
+                Marshal.Copy(dataPtr, packet, 0, 64);
+                byte notifyType = packet[16];
+                short contentFinderConditionId = BitConverter.ToInt16(packet, 38);
+                Task.Run(async delegate
+                {
+                    if (notifyType == 4)
+                    {
+                        var jObject = await XivApi.GetContentFinderCondition(contentFinderConditionId);
+                        var name = ((string)jObject["Name"]).Replace(' ', '_');
+                        var url = $"https://ffxiv.consolegameswiki.com/wiki/{name}#Bosses";
+                        dalamud.Framework.Gui.Chat.Print($"Launching {url}");
+                        Process.Start(new ProcessStartInfo(url)
+                        {
+                            UseShellExecute = true
+                        });
+                    }
+                });
+            }
+        }
+    }
+}
---
+++ XivPlugin.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.CSharp.RuntimeBinder;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+
+namespace Silasary.WikiPlugin
+{
+    public class XivApi
+    {
+        private const string URL = "http://xivapi.com/";
+
+        private static readonly Dictionary<string, JObject> cachedResponses = new Dictionary<string, JObject>();
+
+        public static async Task<JObject> GetContentFinderCondition(int contentFinderCondition)
+        {
+            return await Get("ContentFinderCondition/" + contentFinderCondition, false);
+        }
+
+
+        public static async Task<dynamic> Get(string endpoint, bool noCache = false, params string[] parameters)
+        {
+            string requestParameters = "?";
+            foreach (string str in parameters)
+            {
+                requestParameters = requestParameters + str + "&";
+            }
+            if (cachedResponses.ContainsKey(endpoint + requestParameters) && !noCache)
+            {
+                return cachedResponses[endpoint + requestParameters];
+            }
+            HttpClient httpClient = new HttpClient();
+            JObject jObject = JObject.Parse(await (await httpClient.PostAsync("http://xivapi.com/" + endpoint, new StringContent(requestParameters))).Content.ReadAsStringAsync());
+            if (!noCache)
+            {
+                cachedResponses.Add(endpoint + requestParameters, jObject);
+            }
+            return jObject;
+        }
+    }
+
+}
